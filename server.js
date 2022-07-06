71e8506464e7a444ce2a1a1b6d3170d469ad10ed
Mirza Md.  Moinul Hasan <moinul@akijvg.net>
2022-07-06 12:14:29 +0600

    server added for token

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+    res.send({
+        token: "testToken123"
+    })
+})
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
