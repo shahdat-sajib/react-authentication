aa1583d67793e30bfd51ac965e5f09b162de8ccc
Mirza Md.  Moinul Hasan <moinul@akijvg.net>
2022-07-06 10:50:52 +0600

    dashboard & preferences two components initialized

---
+++ src/components/Preferences/Preferences.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preferences = () => {
+    return (
+        <div>
+            <h2>This is preferences</h2>
+        </div>
+    );
+}
+
+export default Preferences;
