ad8c8ffb7021793fe4b0558ee5f3b7f587d7e6a5
Mirza Md.  Moinul Hasan <moinul@akijvg.net>
2022-07-06 13:05:18 +0600

    async func added for get credentials

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,17 @@ import React, {useState} from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
 
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+   }
+
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
