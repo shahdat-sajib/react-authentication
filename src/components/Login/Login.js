87f0ebcdaf069d54a4f5abe03b4fc7aec1befcc7
Mirza Md.  Moinul Hasan <moinul@akijvg.net>
2022-07-06 12:59:12 +0600

    useState for form data and tokenValue set added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import './Login.css'
+import React, {useState} from 'react';
+import './Login.css';
+import PropTypes from 'prop-types';
 
-const Login = () => {
-    return (
-        <div className='login-wrapper'>
-            <h1>Please login</h1>
-            <form>
-                <label>
-                    <p>Username</p>
-                    <input type="text" />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-
-    );
-}
-
-export default Login;
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+    return(
+      <div className="login-wrapper">
+        <h1>Please Log In</h1>
+        <form>
+          <label>
+            <p>Username</p>
+            <input type="text" onChange={e => setUserName(e.target.value)} />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" onChange={e => setPassword(e.target.value)} />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+  
+  Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  }
