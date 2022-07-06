71853545a969d4c839178d39dfe60d565184e88f
Mirza Md.  Moinul Hasan <moinul@akijvg.net>
2022-07-06 11:56:11 +0600

    useState token use by conditional loop

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
+import Login from './components/Login/Login';
 
 function App() {
+  //for handle token
   const [token, setToken] = useState();
+
+  //show login component when token exist
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  
   return (
     <div className="wrapper">
       <h1>Application</h1>
